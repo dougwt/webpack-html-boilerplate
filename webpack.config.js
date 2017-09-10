6958fe2a8e17099990544c4dbbe0f27ed7824325
Doug Tyler <dougwt@gmail.com>
2017-09-09 23:46:07 -0700

    Add cache busting suffixes to build assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path');
+var webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// A list of 3rd party packages to be bundled separately for caching purposes.
+// Example: [ 'react', 'lodash', 'redux', 'react-redux', 'react-dom' ]
+const VENDOR_LIBS = []
+
 const config = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js'
+  },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'build'),
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -34,11 +41,17 @@ const config = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('style.css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new ExtractTextPlugin('style.[chunkhash].css'),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    }),
+    })
   ]
 };
+
+// Add VENDOR_LIBS to config.entry.vendor if it contains packages
+if (VENDOR_LIBS.length) { config.entry.vendor = VENDOR_LIBS; }
 
 module.exports = config;
